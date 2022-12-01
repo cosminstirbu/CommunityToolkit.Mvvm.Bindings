5c32835d89a5dcbf64ea02311ce25e2d82e539cd
Cosmin Stirbu <stirbucosmin@gmail.com>
2022-12-01 13:39:10 +0200

    Initial commit with the following changes to the original MvvmLight source code
    - Removed extensions related to ObservableCollection
    - Use RelayCommand and RelayCommand<T> from Microsoft.Toolkit.Mvvm
    
    For now, everything else (including namespaces, documentation, class names, etc) remains the same.

---
+++ CommunityToolkit.Mvvm.Bindings/Platforms/iOS/ExtensionsApple.cs
@@ -0,0 +1,110 @@
+﻿// ****************************************************************************
+// <copyright file="ExtensionsApple.cs" company="GalaSoft Laurent Bugnion">
+// Copyright © GalaSoft Laurent Bugnion 2009-2016
+// </copyright>
+// ****************************************************************************
+// <author>Laurent Bugnion</author>
+// <email>laurent@galasoft.ch</email>
+// <date>19.01.2016</date>
+// <project>GalaSoft.MvvmLight</project>
+// <web>http://www.mvvmlight.net</web>
+// <license>
+// See license.txt in this solution or http://www.galasoft.ch/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows.Input;
+using Foundation;
+using Microsoft.Toolkit.Mvvm.Input;
+using UIKit;
+
+namespace GalaSoft.MvvmLight.Helpers
+{
+    /// <summary>
+    /// Defines extension methods for iOS only.
+    /// </summary>
+    ////[ClassInfo(typeof(Binding))]
+    public static class ExtensionsApple
+    {
+        internal static string GetDefaultEventNameForControl(this Type type)
+        {
+            string eventName = null;
+
+            if (type == typeof (UIButton)
+                || typeof (UIButton).IsAssignableFrom(type))
+            {
+                eventName = "TouchUpInside";
+            }
+            else if (type == typeof (UIBarButtonItem)
+                     || typeof (UIBarButtonItem).IsAssignableFrom(type))
+            {
+                eventName = "Clicked";
+            }
+            else if (type == typeof (UISwitch)
+                     || typeof (UISwitch).IsAssignableFrom(type))
+            {
+                eventName = "ValueChanged";
+            }
+
+            return eventName;
+        }
+
+
+        internal static Delegate GetCommandHandler(
+            this EventInfo info,
+            string eventName,
+            Type elementType,
+            ICommand command)
+        {
+            // At the moment, all supported controls with default events
+            // in iOS are using EventHandler, and not EventHandler<...>.
+
+            EventHandler handler = (s, args) =>
+            {
+                if (command.CanExecute(null))
+                {
+                    command.Execute(null);
+                }
+            };
+
+            return handler;
+        }
+
+        internal static Delegate GetCommandHandler<T>(
+            this EventInfo info,
+            string eventName,
+            Type elementType,
+            RelayCommand<T> command,
+            Binding<T, T> castedBinding)
+        {
+            // At the moment, all supported controls with default events
+            // in iOS are using EventHandler, and not EventHandler<...>.
+
+            EventHandler handler = (s, args) =>
+            {
+                var param = castedBinding == null ? default(T) : castedBinding.Value;
+                command.Execute(param);
+            };
+
+            return handler;
+        }
+
+        internal static Delegate GetCommandHandler<T>(
+            this EventInfo info,
+            string eventName,
+            Type elementType,
+            RelayCommand<T> command,
+            T commandParameter)
+        {
+            // At the moment, all supported controls with default events
+            // in iOS are using EventHandler, and not EventHandler<...>.
+
+            EventHandler handler = (s, args) => command.Execute(commandParameter);
+            return handler;
+        }
+    }
+}
